9a95e1d0165ee55cf0b61e9d02198cfb14dd1a6a
Darantar <Darantar@DESKTOP-EK9VSKR>
2024-06-13 01:21:51 +0700

    Third query solution and key refactoring

--- Program.cs
+++ Program.cs
@@ -24,8 +24,8 @@ class Program
         Console.WriteLine("All posts:");
         var data = context.BlogPosts.Select(x => x.Title).ToList();
         Console.WriteLine(JsonSerializer.Serialize(data));
-            
-            
+
+
         Console.WriteLine("How many comments each user left:");
         //ToDo: write a query and dump the data to console
         // Expected result (format could be different, e.g. object serialized to JSON is ok):
@@ -33,8 +33,8 @@ class Program
         // Petr: 2
         // Elena: 3
 
-            Console.WriteLine(
-             JsonSerializer.Serialize(BlogService.NumberOfCommentsPerUser(context)));
+        Console.WriteLine(
+         JsonSerializer.Serialize(BlogService.NumberOfCommentsPerUser(context)));
 
 
 
@@ -63,62 +63,68 @@ class Program
 
     public class BlogService
     {
+
+        //Для полного соответствич "Expected result" можно было упоковать выдачу как примитивы или настроить сериализатор,
+        //но считаю это не совсем рациональным использованием JSON 
+
         public static IQueryable NumberOfCommentsPerUser(MyDbContext context)
         {
             IQueryable myFirstQuery = (from comments in context.BlogComments //Можно было добавить <dynamic>, но задание не подорузумевает неккоректных данных другого из БД
-                                group comments by comments.UserName into dump
-                                select new { UserName = dump.Key, Count = dump.Count()});
+                                       group comments by comments.UserName into dump
+                                       select new { UserName = dump.Key, UserCommentsCount = dump.Count() });
             return myFirstQuery;
         }
 
         public static IQueryable PostsOrderedByLastCommentDate(MyDbContext context)
         {
             IQueryable mySecondQuery = (from posts in context.BlogPosts
-                                 let lastComment = posts.Comments.OrderByDescending(c => c.CreatedDate).FirstOrDefault()
-                                 let lastCommentDate = lastComment.CreatedDate.Date
-                                 orderby lastCommentDate descending
-                                 select new { PostName = posts.Title, LastCommentDate = lastCommentDate, LastCommentText = lastComment.Text });
+                                        let lastComment = posts.Comments.OrderByDescending(c => c.CreatedDate).FirstOrDefault()
+                                        let lastCommentDate = lastComment.CreatedDate.Date
+                                        orderby lastCommentDate descending
+                                        select new { PostName = posts.Title, LastPostCommentDate = lastCommentDate, LastPostCommentText = lastComment.Text });
             return mySecondQuery;
         }
 
         public static IQueryable NumberOfLastCommentsLeftByUser(MyDbContext context)
         {
             IQueryable myThirdQuery = (from posts in context.BlogPosts
-                                        let lastComment = posts.Comments.OrderByDescending(c => c.CreatedDate).FirstOrDefault()
-                                        let lastCommentDate = lastComment.CreatedDate.Date
-                                        orderby lastCommentDate descending
-                                        select new { PostName = posts.Title, LastCommentDate = lastCommentDate, LastCommentText = lastComment.Text });
+                                       let lastComment = posts.Comments.OrderByDescending(c => c.CreatedDate).FirstOrDefault()
+                                       group posts by lastComment.UserName into dump
+
+                                       select new { UserName = dump.Key, UserLastPostCommentsCount = dump.Count() });
             return myThirdQuery;
         }
-    private static void InitializeData(MyDbContext context)
-    {
-        context.BlogPosts.Add(new BlogPost("Post1")
+    }
+        private static void InitializeData(MyDbContext context)
         {
-            Comments = new List<BlogComment>()
+            context.BlogPosts.Add(new BlogPost("Post1")
+            {
+                Comments = new List<BlogComment>()
             {
                 new BlogComment("1", new DateTime(2020, 3, 2), "Petr"),
                 new BlogComment("2", new DateTime(2020, 3, 4), "Elena"),
                 new BlogComment("8", new DateTime(2020, 3, 5), "Ivan"),
             }
-        });
-        context.BlogPosts.Add(new BlogPost("Post2")
-        {
-            Comments = new List<BlogComment>()
+            });
+            context.BlogPosts.Add(new BlogPost("Post2")
+            {
+                Comments = new List<BlogComment>()
             {
                 new BlogComment("3", new DateTime(2020, 3, 5), "Elena"),
                 new BlogComment("4", new DateTime(2020, 3, 6), "Ivan"),
             }
-        });
-        context.BlogPosts.Add(new BlogPost("Post3")
-        {
-            Comments = new List<BlogComment>()
+            });
+            context.BlogPosts.Add(new BlogPost("Post3")
+            {
+                Comments = new List<BlogComment>()
             {
                 new BlogComment("5", new DateTime(2020, 2, 7), "Ivan"),
                 new BlogComment("6", new DateTime(2020, 2, 9), "Elena"),
                 new BlogComment("7", new DateTime(2020, 2, 10), "Ivan"),
                 new BlogComment("9", new DateTime(2020, 2, 14), "Petr"),
             }
-        });
-        context.SaveChanges();
-    }
+            });
+            context.SaveChanges();
+        }
+    
 }
